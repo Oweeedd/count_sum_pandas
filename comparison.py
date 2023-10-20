11e6d811228d4a726d5df480f4a59ec5aa955b7c
Oveedd <85689777+Oweeedd@users.noreply.github.com>
2023-10-20 18:42:45 +0300

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,13 +1,17 @@
-def compare(expression):
-    parts = expression.split('>')
+def compare(input_str):
+    
+    parts = input_str.split()
 
-    if len(parts) != 2:
-        return False
+    if len(parts) != 3:
+        return False  
 
-    try:
-        left = int(parts[0])
-        right = int(parts[1])
-    except ValueError:
-        return False
+    operand1 = int(parts[0])
+    operator = parts[1]
+    operand2 = int(parts[2])
 
-    return left > right
+    if operator == "<":
+        return operand1 < operand2
+    elif operator == ">":
+        return operand1 > operand2
+    else:
+        return False  
