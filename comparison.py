f66e71740f1f165f2f685208d40570fe698d1c89
Oveedd <85689777+Oweeedd@users.noreply.github.com>
2023-10-20 18:59:20 +0300

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,15 +1,20 @@
 def compare(input_str):
-    
+
     parts = input_str.split()
 
     if len(parts) != 3:
-        return False  
+        return False
 
-    operand1 = int(parts[0])
-    operator = parts[1]
-    operand2 = int(parts[2])
+    try:
+        operand1 = int(parts[0])
+        operator = parts[1]
+        operand2 = int(parts[2])
+    except ValueError:
+        return False
 
     if operator == "<":
         return operand1 < operand2
-    else operator == ">":
+    elif operator == ">":
         return operand1 > operand2
+    else:
+        return False
