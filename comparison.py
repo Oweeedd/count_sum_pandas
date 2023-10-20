3d467655037641699c6a14cc21bd31027f4e46b8
Oveedd <85689777+Oweeedd@users.noreply.github.com>
2023-10-20 19:25:36 +0300

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,17 +1,6 @@
 def compare(expression):
-
-    parts = expression.split()
-
-    if len(parts) != 3:
-        raise ValueError("тесты у вас отличные!!!")
-
-    left = int(parts[0])
-    operator = parts[1]
-    right = int(parts[2])
-
-    if operator == ">":
-        return left > right
-    elif operator == "<":
-        return left < right
-    else:
-        raise ValueError("тесты у вас отличные!!!")
+    try:
+        return eval(expression)
+    except Exception as e:
+        print(f"{e}")
+        return False
